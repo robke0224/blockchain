3dfe9ed7ecabda51cdf208f6c23f24b0e07d3687
robke0224 <154459735+robke0224@users.noreply.github.com>
2024-09-25 02:06:14 +0300

    Update hash.h

--- hash.h
+++ hash.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <fstream>
+#include <random>
 
 using namespace std;
 
@@ -13,5 +14,10 @@ string sesiolika_bitu(string& bits, const string& originalInput);
 int suma(const string& input);
 string dauginti_bitus_is_sumos(string bits, int wordSum);
 void apdoroja(const string& input, ofstream& outputFile);
+void create_single_char_file(const std::string& filename, char c, size_t size);
+void create_random_char_file(const std::string& filename, size_t size);
+void create_similar_files(const std::string& filename1, const std::string& filename2, size_t size);
+void create_empty_file(const std::string& filename);
+void performance_test(const std::string& filename, int lineCount);
 
 #endif
