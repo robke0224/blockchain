d9e4f34845fb0a317f77d9f205ea00b9afbd3a0d
robke0224 <154459735+robke0224@users.noreply.github.com>
2024-09-26 23:29:22 +0300

    Update hash.h

--- hash.h
+++ hash.h
@@ -22,5 +22,9 @@ void testukas1(const std::string& filename, int lineCount);
 string poros_random(int length);
 void kolizijos(const vector<pair<string, string> >& stringPairs);
 vector<pair<string, string> > loadStringPairsFromFile(const string& filename);
+string generate_random_string(size_t length);
+double compute_bit_difference(const string &binary1, const string &binary2);
+double compute_hex_difference(const string &hex1, const string &hex2);
+
 
 #endif
