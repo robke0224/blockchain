5ad8ddba10318ca032d9b5f1c23c0d2773e3934a
robke0224 <154459735+robke0224@users.noreply.github.com>
2024-09-26 22:23:15 +0300

    -collision resistance testavimas

--- hash.h
+++ hash.h
@@ -19,5 +19,8 @@ void kratinukas(const std::string& filename, size_t size);
 void nevienodi(const std::string& filename1, const std::string& filename2, size_t size);
 void tuscias(const std::string& filename);
 void testukas1(const std::string& filename, int lineCount);
+string poros_random(int length);
+void kolizijos(const vector<pair<string, string> >& stringPairs);
+vector<pair<string, string> > loadStringPairsFromFile(const string& filename);
 
 #endif
